e10ad0f1844a1d3c48616a147b71d3d76a394a27
marc condon <marc@fogtest.com>
2017-01-26 11:07:27 -0800

    corrected call to generatepdf in /invoices/send/<invoice_id>

--- app/controllers/invoices_controller.php
+++ app/controllers/invoices_controller.php
@@ -261,9 +261,7 @@ class InvoicesController extends AppController {
             $this->Email->fromName     = "Rockets Redglare A/R";
             
             // $this->Email->body = $filename;
-            $this->generatepdf($invoice,$client,$employee);
-
-
+            $this->Invoice->generatepdf($invoice,$client,$employee);
 			
 	    $subject = $this->invoiceFunction->email_subject($invoice, $employee);
 	    // $filename =  $this->invoiceFunction->invoiceFilename($invoice,$employee);
