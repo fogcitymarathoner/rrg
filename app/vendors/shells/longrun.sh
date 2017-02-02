c27c26a61d952bd1e82e85742ca263a607887ad3
marc condon <marc@fogtest.com>
2017-02-02 15:25:54 -0800

    removed a shell script called longrun

--- app/vendors/shells/longrun.sh
+++
@@ -1,9 +0,0 @@
-# generate the tags
-echo generate the tags
-php ~/cake/console/cake.php -app ~/CakeRRGWorkspace/cakerrg/app generate_commissions_reports_tags
-# tag commissions/notes items
-echo commissions/notes items
-php ~/cake/console/cake.php -app ~/CakeRRGWorkspace/cakerrg/app commissions_items_fix_reports_tags
-# generate the reports
-echo generate the reports
-php ~/cake/console/cake.php -app ~/CakeRRGWorkspace/cakerrg/app generate_tag_reports
